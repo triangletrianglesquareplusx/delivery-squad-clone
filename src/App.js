d931d8a5a321f09b46f106c57984dadfae946cfa
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-09-15 08:49:52 +0300

    second test, so far no result from vercel

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and test whether Vercel will update.
+          Learn React and test whether Vercel will update, perhaps not.
         </a>
       </header>
     </div>
