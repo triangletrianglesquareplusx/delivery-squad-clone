02eddd2fd7e59c86841cc0142200bdd243179bbe
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-09-28 12:54:25 +0300

    smooth animation done, animate presence used

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Articles from "./pages/Articles";
 import SingleArticle from "./pages/SingleArticle";
 import RestaurantsFullPage from "./pages/RestaurantsFullPage";
@@ -13,27 +13,29 @@ import RegistrationPage from "./pages/RegistrationPage";
 import AdminPage from "./pages/AdminPage";
 import ErrorPage from "./pages/ErrorPage";
 import PrivateRoutes from "./utilities/PrivateRoutes";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
     <>
       <Header />
-
-      <Routes>
-        {/*<Route path="/" element={<Hackathon />}/>*/}
-        <Route element={<PrivateRoutes />}>
-          <Route path="/admin" element={<AdminPage />} />
-        </Route>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<RegistrationPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/articles" element={<Articles />} />
-        <Route path="/articles/:slug" element={<SingleArticle />} />
-        <Route path="/restaurants" element={<RestaurantsFullPage />} />
-        <Route path="/shopping" element={<Shopping />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/error" element={<ErrorPage />} />
-      </Routes>
+      <AnimatePresence exitBeforeEnter>
+        <Routes key={location.pathname} location={location}>
+          <Route element={<PrivateRoutes />}>
+            <Route path="/admin" element={<AdminPage />} />
+          </Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/articles/:slug" element={<SingleArticle />} />
+          <Route path="/restaurants" element={<RestaurantsFullPage />} />
+          <Route path="/shopping" element={<Shopping />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/error" element={<ErrorPage />} />
+        </Routes>
+      </AnimatePresence>
       <Footer />
     </>
   );
