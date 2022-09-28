b40350603b6b1d82a682b7d5b9194a7bcaed537a
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-09-28 12:49:15 +0300

    added basic animation

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,6 +8,7 @@ import auth from "../firebase/firebase-config";
 import { loginUser } from "../features/authSlice";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import AnimatedPage from "../components/animations-test/AnimatedPage";
 function LoginPage() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -53,44 +54,48 @@ function LoginPage() {
   };
 
   return (
-    <div className="flex items-start justify-center h-screen m-10">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="container flex flex-col items-center w-4/12 gap-2 p-10 text-sm rounded-lg shadow-lg bg-scaledDownWhite"
-      >
-        <div className="flex justify-center gap-3 text-regalBlue">
-          <HiLightBulb />
-          <p className="text-sm text-black">
-            Welcome back <span className="text-sm text-regalBlue">friend</span>{" "}
-            - enter.
-          </p>
-        </div>
-        <p className="text-sm text-center">Email address</p>
-        <div className="flex flex-col w-9/12">
-          <input
-            {...register("email", { required: true })}
-            className="p-2 bg-blue-200 rounded-lg outline-none"
-          />
-        </div>
-        <p className="text-sm text-center">Password</p>
-        <div className="flex flex-col w-9/12 gap-2">
-          <input
-            {...register("passwordRequired", { required: true })}
-            className="p-2 bg-blue-200 rounded-lg outline-none"
-          />
-          <p className="text-red-600 ">{errors.passwordRequired?.message}</p>
-        </div>
-        <Link to="/register">
-          <p className="text-xs text-regalBlue">Not a friend yet? Register!</p>
-        </Link>
+    <AnimatedPage>
+      <div className="flex items-start justify-center h-screen m-10">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="container flex flex-col items-center w-4/12 gap-2 p-10 text-sm rounded-lg shadow-lg bg-scaledDownWhite"
+        >
+          <div className="flex justify-center gap-3 text-regalBlue">
+            <HiLightBulb />
+            <p className="text-sm text-black">
+              Welcome back{" "}
+              <span className="text-sm text-regalBlue">friend</span> - enter.
+            </p>
+          </div>
+          <p className="text-sm text-center">Email address</p>
+          <div className="flex flex-col w-9/12">
+            <input
+              {...register("email", { required: true })}
+              className="p-2 bg-blue-200 rounded-lg outline-none"
+            />
+          </div>
+          <p className="text-sm text-center">Password</p>
+          <div className="flex flex-col w-9/12 gap-2">
+            <input
+              {...register("passwordRequired", { required: true })}
+              className="p-2 bg-blue-200 rounded-lg outline-none"
+            />
+            <p className="text-red-600 ">{errors.passwordRequired?.message}</p>
+          </div>
+          <Link to="/register">
+            <p className="text-xs text-regalBlue">
+              Not a friend yet? Register!
+            </p>
+          </Link>
 
-        {/*<input type="submit" value="Login" />*/}
-        <ControlButton
-          name="Login"
-          className="px-5 py-1 text-white rounded-md shadow-lg bg-regalBlue"
-        />
-      </form>
-    </div>
+          {/*<input type="submit" value="Login" />*/}
+          <ControlButton
+            name="Login"
+            className="px-5 py-1 text-white rounded-md shadow-lg bg-regalBlue"
+          />
+        </form>
+      </div>
+    </AnimatedPage>
   );
 }
 
--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import auth from "../firebase/firebase-config";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { ImEnter } from "react-icons/im";
-
+import AnimatedPage from "../components/animations-test/AnimatedPage";
 function RegistrationPage() {
   const navigate = useNavigate();
 
@@ -27,58 +27,65 @@ function RegistrationPage() {
   };
 
   return (
-    <div>
-      <div className="flex items-start justify-center h-screen m-10">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="container flex flex-col items-center w-4/12 gap-2 p-10 text-sm rounded-lg shadow-lg bg-scaledDownWhite"
-        >
-          <div className="flex items-center justify-center gap-3 text-regalBlue">
-            <ImEnter />
-            <p className="text-sm text-black">
-              Register and become a
-              <span className="text-sm text-regalBlue"> friend</span>.
-            </p>
-          </div>
-          <p className="text-sm text-center">Email address</p>
-          <div className="flex flex-col w-9/12">
-            <input
-              {...register("emailRegister", {
-                required: {
-                  value: true,
-                  message: "This field is mandatory!",
-                },
-              })}
-              className="p-2 bg-blue-200 rounded-lg outline-none"
-            />
-            <p className="text-center text-red-600">
-              {errors.emailRegister?.message}
-            </p>
-          </div>
-          <p className="text-sm text-center">Password</p>
-          <div className="flex flex-col w-9/12 gap-2">
-            <input
-              {...register("passwordRegister", {
-                required: true,
-                minLength: {
-                  value: 10,
-                  message: "Min. length of 10 required!",
-                },
-                pattern: {
-                  value: /\d+/,
-                  message: "Must be only integers!",
-                },
-              })}
-              className="p-2 bg-blue-200 rounded-lg outline-none"
-            />
-            <p className="text-center text-red-600">
-              {errors.passwordRegister?.message}
-            </p>
-          </div>
-          <input type="submit" value="Register" />
-        </form>
+    <AnimatedPage>
+      <div>
+        <div className="flex items-start justify-center h-screen m-10">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="container flex flex-col items-center w-4/12 gap-2 p-10 text-sm rounded-lg shadow-lg bg-scaledDownWhite"
+          >
+            <div className="flex items-center justify-center gap-3 text-regalBlue">
+              <ImEnter />
+              <p className="text-sm text-black">
+                Register and become a
+                <span className="text-sm text-regalBlue"> friend</span>.
+              </p>
+            </div>
+            <p className="text-sm text-center">Email address</p>
+            <div className="flex flex-col w-9/12">
+              <input
+                {...register("emailRegister", {
+                  required: {
+                    value: true,
+                    message: "This field is mandatory!",
+                  },
+                })}
+                className="p-2 bg-blue-200 rounded-lg outline-none"
+              />
+              <p className="text-center text-red-600">
+                {errors.emailRegister?.message}
+              </p>
+            </div>
+            <p className="text-sm text-center">Password</p>
+            <div className="flex flex-col w-9/12 gap-2">
+              <input
+                {...register("passwordRegister", {
+                  required: true,
+                  minLength: {
+                    value: 10,
+                    message: "Min. length of 10 required!",
+                  },
+                  pattern: {
+                    value: /\d+/,
+                    message: "Must be only integers!",
+                  },
+                })}
+                className="p-2 bg-blue-200 rounded-lg outline-none"
+              />
+              <p className="text-center text-red-600">
+                {errors.passwordRegister?.message}
+              </p>
+            </div>
+            <Link to="/login">
+              <p className="text-xs text-regalBlue">
+                Wanted to login? Go back.
+              </p>
+            </Link>
+            <input type="submit" value="Register" />
+          </form>
+        </div>
       </div>
-    </div>
+    </AnimatedPage>
   );
 }
 
